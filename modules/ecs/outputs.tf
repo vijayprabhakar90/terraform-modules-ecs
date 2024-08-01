aaab13a472104a3922d8922320d421ac22bcc2ca
Vijay <vijay.subbaraj@symphony.com>
2024-08-01 22:30:11 +0800

    few changes in aws resources

--- modules/ecs/outputs.tf
+++ modules/ecs/outputs.tf
@@ -2,6 +2,6 @@ output "target_group_arns" {
   value = aws_lb_target_group.tg[*].arn
 }
 
-output "ecs_service_arns" {
-  value = aws_ecs_service.main[*].arn
+output "ecs_service_ids" {
+  value = aws_ecs_service.main[*].id
 }

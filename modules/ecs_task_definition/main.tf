75df354713a7692cdc8304a1b377e49e4710ce49
Vijay <vijay.subbaraj@symphony.com>
2024-08-06 18:34:58 +0800

    Added task definition file for ecs deployment

--- modules/ecs_task_definition/main.tf
+++ modules/ecs_task_definition/main.tf
@@ -1,11 +1,8 @@
 resource "aws_ecs_task_definition" "main" {
   family                = var.family
   container_definitions = var.container_definitions
-  network_mode          = "awsvpc"
 
   requires_compatibilities = ["EC2"]
-  cpu                      = var.cpu
-  memory                   = var.memory
   execution_role_arn       = var.execution_role_arn
   task_role_arn            = var.task_role_arn
 
